f64a863e35357491f96670f7737d4d3c26022cc1
Frederico Enzo <portifolio.frederico@gmail.com>
2023-10-04 21:12:50 -0300

    Carros e LIvreos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { PessoaslistComponent } from './pessoas/pessoaslist/pessoaslist.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
+import { CarroslistComponent } from './carros/carroslist/carroslist.component';
+import { CarrosdetailsComponent } from './carros/carrosdetails/carrosdetails.component';
+import { LivroslistComponent } from './livros/livroslist/livroslist.component';
+import { LivrosdatailsComponent } from './livros/livrosdatails/livrosdatails.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PessoaslistComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    CarroslistComponent,
+    CarrosdetailsComponent,
+    LivroslistComponent,
+    LivrosdatailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/livros/livrosdatails/livrosdatails.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-livrosdatails',
+  templateUrl: './livrosdatails.component.html',
+  styleUrls: ['./livrosdatails.component.scss']
+})
+export class LivrosdatailsComponent {
+
+}
